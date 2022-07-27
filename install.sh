9315099c6ee4dbc4a07ff18e3d91c96a2c341d4a
jailondanet2 <109008134+jailondanet2@users.noreply.github.com>
2022-07-27 13:05:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ echo -e "\033[1;33m AGUARDE..."
 echo ""
 
 cd /var/www/html
-wget https://www.dropbox.com/s/sjzgmbjkrzxa5wc/v20.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/m5oieltjp77kv1a/v20.zip > /dev/null 2>&1
 unzip v20.zip > /dev/null 2>&1
 rm -rf v20.zip index.html > /dev/null 2>&1
 service apache2 restart > /dev/null 2>&1
